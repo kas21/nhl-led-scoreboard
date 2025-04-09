162fe02c0fa366bcde12edfe1b7f6e337891cf51
Kevin Shipley <kevinashipley@gmail.com>
2025-04-09 08:45:13 -0400

    updated font on stats_leaders and cleaned up ovi tracker

--- src/boards/ovi_tracker.py
+++ src/boards/ovi_tracker.py
@@ -15,12 +15,9 @@ class OviTrackerRenderer:
         self.layout = self.get_layout()
 
         self.team_colors = data.config.team_colors
-        self.font_large_2 = data.config.layout.font_large_2
-        self.font_large = data.config.layout.font_large
-        self.font = data.config.layout.font
         
         # Gretzky's career goals record
-        self.GRETZKY_GOALS = 894
+        #self.GRETZKY_GOALS = 894
         self.OVI_ID = "8471214"  # Ovechkin's NHL ID
         self.team_id = 15 # Capitals
 
@@ -55,9 +52,6 @@ class OviTrackerRenderer:
             bg_color = team_colors.color("{}.primary".format(team_id))
             txt_color = team_colors.color("{}.text".format(team_id))
 
-            letter_height = 6
-            letter_spacing = 1
-
             # Render logo
             logo_renderer = LogoRenderer(
                 self.matrix,
@@ -70,31 +64,16 @@ class OviTrackerRenderer:
 
             # Clear the matrix
             self.matrix.clear()
-
-            # gradient = Image.open(get_file('assets/images/64x32_scoreboard_center_gradient.png'))
-
-            # #   For 128x64 use the bigger gradient image and larger font
-            # if self.matrix.height == 64:
-            #     gradient = Image.open(get_file('assets/images/128x64_scoreboard_center_gradient.png'))
-            #     self.font = self.data.config.layout.font_large
-            #     letter_height = 12
             
             logo_renderer.render()
-            # self.matrix.draw_image((20,0), gradient, align="center")
-            
             # Draw stats
             current_y = 2
-            # self.matrix.draw_text(
-            #     ("5%", current_y),
-            #     f"NHL ALL-TIME GOALS LEADER",
-            #     font=self.font,
-            #     fill=(255,255,255)
-            # ) 
+
             self.matrix.draw_text_layout(
                 self.layout.title,
                 self.title_text
             )
-            current_y += letter_height + letter_spacing + 1
+ 
             self.matrix.draw_text_layout(
                 self.layout.name,
                 f"ALEX OVECHKIN",
@@ -106,25 +85,8 @@ class OviTrackerRenderer:
                 f"{stats.career_goals}"
             )
 
-            # current_y += letter_height + letter_spacing + 1
-            # # Calculate and draw goals needed
-            # goals_needed = self.GRETZKY_GOALS - stats.career_goals + 1
-            # self.matrix.draw_text(
-            #     (1, current_y),
-            #     f"To Go",
-            #     font=self.font,
-            #     fill=(txt_color['r'], txt_color['g'], txt_color['b']),
-            #     backgroundColor=(bg_color['r'], bg_color['g'], bg_color['b'])
-            # )
-            # current_y += letter_height + letter_spacing
-            # self.matrix.draw_text(
-            #     (1, current_y),
-            #     f"{goals_needed}",
-            #     font=self.font
-            # )
-
             # Render to matrix
             self.matrix.render()
-            self.sleepEvent.wait(15)
+            self.sleepEvent.wait(8)
         except Exception as e:
             debug.error(f"Error rendering Ovi tracker: {str(e)}\n{traceback.format_exc()}") 
